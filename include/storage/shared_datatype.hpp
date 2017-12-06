c3ac00311bf1846e8f8ede96f3771d3a955ebb95
Daniel Patterson <daniel@mapbox.com>
2017-12-05 22:44:51 -0800

    Test cases all work, hooray

--- include/storage/shared_datatype.hpp
+++ include/storage/shared_datatype.hpp
@@ -97,7 +97,8 @@ const constexpr char *block_id_to_name[] = {"NAME_CHAR_DATA",
                                             "MLD_GRAPH_NODE_LIST",
                                             "MLD_GRAPH_EDGE_LIST",
                                             "MLD_GRAPH_NODE_TO_OFFSET",
-                                            "MANEUVER_OVERRIDES"};
+                                            "MANEUVER_OVERRIDES",
+                                            "MANEUVER_OVERRIDE_NODE_SEQUENCES"};
 
 struct DataLayout
 {
@@ -183,6 +184,7 @@ struct DataLayout
         MLD_GRAPH_EDGE_LIST,
         MLD_GRAPH_NODE_TO_OFFSET,
         MANEUVER_OVERRIDES,
+        MANEUVER_OVERRIDE_NODE_SEQUENCES,
         NUM_BLOCKS
     };
 
